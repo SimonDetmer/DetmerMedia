e65998416f85d897f074fdf992eaeaa8a645a721
simon <simon.detmer@googlemail.com>
2025-03-28 11:09:31 +0100

    Initiale Version

---
+++ script.js
@@ -0,0 +1,10 @@
+// Beispielhafte Funktion für zukünftige JavaScript-Features
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Die Seite ist bereit!");
+
+    // Zum Beispiel könnte hier eine Interaktivität hinzugefügt werden
+    const submitButton = document.querySelector("button[type='submit']");
+    submitButton.addEventListener("click", function(event) {
+        alert("Formular wurde abgesendet!");
+    });
+});
